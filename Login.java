585e7a1ac648a788b643cbf338fff4d9967d2d96
Hugo23-4 <hugoestudiospm1@gmail.com>
2025-01-22 03:59:25 +0100

    Create Login.java

---
+++ Login.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Login {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Bienbenido al sistema de gestion de libros de la niblioteca municipal de Merida");
+    
+}
